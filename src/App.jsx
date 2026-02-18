2032489f3649dc34e84a8ed2ad52ac315698a044
unknown <nylonstripe5u@gmail.com>
2026-02-18 11:41:32 +0530

    add task card

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ import './App.css'
 function App() {
 
   return (
-    <div>
+    <div className='page'>
         <header>
             <div>Snippet Vault</div>
         </header>
-        <section>
+        <section className=''>
             <div className='card'>
-                <h5>Make a new Snippet</h5>
-                <input type='text' placeholder='Give snippet a title'></input>
-                <input type='textarea' placeholder='Paste snippet'></input>
-                <button>Add Snippet</button>
+                <input type='text' className='input-title card-border' placeholder='Give snippet a title'></input>
+                <textarea className='input-snippet card-border' placeholder='Paste snippet' rows={10}></textarea>
+                <button className='add-snippet-btn card-border' >Add Snippet</button>
             </div>
         </section>
 
